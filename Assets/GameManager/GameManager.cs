0fe7af87ddad493dcc6ac2201fa95a359a0e2504
MichaelWkTran <Michael.wktran@gmail.com>
2022-10-04 08:15:19 +1300

    Added endless levels and pausing

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -6,19 +6,60 @@ public class GameManager : MonoBehaviour
 {
     public uint score;
     public Cinemachine.CinemachineVirtualCamera virtualCamera;
+    public bool paused { get; private set; } = false;
+
+    [Header("Level")]
+    public GameObject[] levelSegments;
+    [SerializeField] Transform gameWorld;
+    [SerializeField] float loopDistance;
+
+    [Header("Death Wall")]
+    [SerializeField] Transform deathWall;
+    [SerializeField] float deathWallMoveSpeed;
+    [SerializeField] float deathWallAcceleration;
 
     [Header("UI")]
     [SerializeField] TMPro.TMP_Text scoreUI;
+    [SerializeField] GameObject pauseScreen;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         scoreUI.text = score.ToString();
+
+        //Shift objects back if the camera goes further than loopDistance
+        if (Camera.main.transform.position.x > loopDistance)
+        {
+            //Shift all objects in world back by loopDistance
+            foreach (Transform worldtransforms in gameWorld)
+            {
+                worldtransforms.position -= Vector3.right * loopDistance;
+            }
+
+            //Shift virtualCamera in world back by loopDistance
+            virtualCamera.ForceCameraPosition(virtualCamera.transform.position -= Vector3.right * loopDistance, Quaternion.identity);
+        }
+
+        //Move Death Wall
+        deathWall.transform.position += Vector3.right * deathWallMoveSpeed * Time.deltaTime;
+        deathWallMoveSpeed += deathWallAcceleration;
+    }
+
+    public void Pause()
+    {
+        paused = true;
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void UnPause()
+    {
+        paused = false;
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
---
+++ Assets/LevelSegment.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSegment : MonoBehaviour
+{
+    [SerializeField] float segmentWidth; //How is the level segment
+    [SerializeField] bool createdNextSegment = false; //Whether the next level segment has been created or not
+    static float segmentGizmoHeight = 10.0f;  //The height of the level segment box displayed in gizmos
+    GameManager gameManager;
+    
+    void Start()
+    {
+        //Get the game manager
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    void Update()
+    {
+        //If the level segment appears on screen, create the next segment
+        if (!createdNextSegment && transform.position.x < Camera.main.transform.position.x + (Camera.main.orthographicSize * Camera.main.aspect))
+        {
+            createdNextSegment = true;
+            Instantiate
+            (
+                gameManager.levelSegments[Random.Range(0, gameManager.levelSegments.Length)],
+                transform.position + (Vector3.right * segmentWidth), Quaternion.identity
+            ).transform.parent = transform.parent;
+        }
+
+        //Destroy the segment if it goes off screen
+        if (transform.position.x + segmentWidth < Camera.main.transform.position.x - (Camera.main.orthographicSize * Camera.main.aspect))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube
+        (
+            transform.position + (new Vector3(segmentWidth, segmentGizmoHeight) /2.0f),
+            new Vector3(segmentWidth, segmentGizmoHeight, 1.0f)
+        );
+    }
+}
